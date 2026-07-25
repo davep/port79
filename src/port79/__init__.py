76f6dd7a644fb53f60cf08427982825d40eebc5a
Dave Pearson <davep@davep.org>
2026-07-25 11:44:49 +0100

    :card_index: Add the usual library metadata

--- src/port79/__init__.py
+++ src/port79/__init__.py
@@ -1,8 +1,18 @@
 """An async-first, fully-typed client library for the Finger protocol (RFC 1288)."""
 
 ##############################################################################
-# Future imports.
-from __future__ import annotations
+# Python imports.
+from importlib.metadata import version
+
+######################################################################
+# Main library information.
+__author__ = "Dave Pearson"
+__copyright__ = "Copyright 2026, Dave Pearson"
+__credits__ = ["Dave Pearson"]
+__maintainer__ = "Dave Pearson"
+__email__ = "davep@davep.org"
+__version__: str = version("port79")
+__licence__ = "MIT"
 
 ##############################################################################
 # Local imports.
@@ -24,6 +34,7 @@ from .uri import (
 )
 
 ##############################################################################
+# Public API.
 __all__ = [
     "Client",
     "ConnectionError",
